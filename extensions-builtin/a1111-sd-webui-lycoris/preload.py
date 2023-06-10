a40f031f2c66273f11249381d7caa5d584272dfe
Bagasekaaprs <136058556+21sysai@users.noreply.github.com>
2023-06-10 12:10:28 +0700

    Delete extensions-builtin/a1111-sd-webui-lycoris directory

--- extensions-builtin/a1111-sd-webui-lycoris/preload.py
+++
@@ -1,6 +0,0 @@
-import os
-from modules import paths
-
-
-def preload(parser):
-    parser.add_argument("--lyco-dir", type=str, help="Path to directory with LyCORIS networks.", default=os.path.join(paths.models_path, 'LyCORIS'))
